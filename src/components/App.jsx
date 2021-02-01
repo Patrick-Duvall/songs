9f012479560769e469b703ce62732c3db55da638
Patrick Duvall <duvallp208@gmail.com>
2021-02-01 13:45:08 -0700

    Add list of songs to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,13 @@ import { selectSong } from '../actions/index'
 import SongList from './SongList'
 
 const App = () => {
-  console.log('app loaded');
   return(
-    <div>
-      <SongList/>
+    <div className='ui container grid'>
+      <div className='ui row'>
+        <div className='column eight wide'>
+          <SongList/>
+        </div>
+      </div>
     </div>
   )
 }
